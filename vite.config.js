24c48e7cd0dee2606c0dd9b8508c4008862e0a3e
Dishank-Sen <dishanksen05@gmail.com>
2025-02-11 19:23:51 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './public', // Set root to public since index.html is there
+  root: './', // Set root to public since index.html is there
   build: {
     outDir: '../dist', // Ensure build output is outside 'public'
     emptyOutDir: true, // Cleans the output directory before building
     rollupOptions: {
-      input: 'index.html' // Ensure index.html is inside public
+      input: './public/index.html' // Ensure index.html is inside public
     }
   },
   server: {
