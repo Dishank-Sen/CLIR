ad181ec380ea94566b3007f7ee8bcd8c61a559de
Dishank-Sen <dishanksen05@gmail.com>
2025-02-11 19:06:17 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './public', // Keep this if your root is public
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
   },
+  server: {
+    historyApiFallback: true
+  }
 });
